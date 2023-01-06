6aaab8a02ac5bf6e23e3fe623324bc95090cbee4
VitorGuedesMadeira <vgm_rox@hotmail.com>
2023-01-06 19:02:35 -0300

    Add color picker

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,4 +1,6 @@
+/* eslint-disable no-unused-expressions */
 import React, { useState } from 'react';
+import { CirclePicker } from 'react-color';
 
 // styles
 import './Editor.css';
@@ -8,16 +10,18 @@ const Editor = () => {
   const [panelHeight, setPanelHeight] = useState(16);
   const [hideOptions, setHideOptions] = useState(false);
   const [hideDrawingPanel, setDrawingPanel] = useState(true);
-  const [buttonText, setButtonText] = useState("Start Drawing");
-  const [selectedColor, setColor] = useState("#f44336");
+  const [buttonText, setButtonText] = useState('Start Drawing');
+  const [selectedColor, setColor] = useState('#f44336');
 
   const initializeDrawingPanel = () => {
     setHideOptions(!hideOptions);
     setDrawingPanel(!hideDrawingPanel);
+    buttonText === 'Start Drawing' ? setButtonText('Reset') : setButtonText('Start Drawing');
+  };
 
-    buttonText === "Start Drawing" ? setButtonText("Reset") : setButtonText("Start Drawing")
-
-  }
+  const changeColor = (color) => {
+    setColor(color.hex);
+  };
 
   return (
     <div className="editor">
@@ -26,29 +30,32 @@ const Editor = () => {
       {hideDrawingPanel && (
       <div className="options">
         <div className="option">
-          <input 
-            type="number" 
-            className="panel-input" 
-            defaultValue={panelWidth} 
-            onChange={(e) => {setPanelWidth(e.target.value)}}
+          <input
+            type="number"
+            className="panel-input"
+            defaultValue={panelWidth}
+            onChange={(e) => { setPanelWidth(e.target.value); }}
           />
         </div>
         <span>Width</span>
 
         <div className="option">
-          <input 
-            type="number" 
-            className="panel-input" 
-            defaultValue={panelHeight} 
-            onChange={(e) => {setPanelHeight(e.target.value)}}
+          <input
+            type="number"
+            className="panel-input"
+            defaultValue={panelHeight}
+            onChange={(e) => { setPanelHeight(e.target.value); }}
           />
         </div>
         <span>Height</span>
-      </div>)}
+      </div>
+      )}
 
-      <button onClick={initializeDrawingPanel} className="button">{buttonText}</button>
+      <button type="button" onClick={initializeDrawingPanel} className="button">{buttonText}</button>
+
+      {hideOptions && <CirclePicker color={selectedColor} onChangeComplete={changeColor} />}
     </div>
-  )
+  );
 };
 
 export default Editor;
